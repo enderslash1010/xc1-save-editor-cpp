fce2cb12ecf1770a738a872c126af71f7d4f7ce4
enderslash1010 <64379743+enderslash1010@users.noreply.github.com>
2024-03-09 22:48:01 -0600

    Added DataObject class

---
+++ include/DataObject.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "SaveConfig.h"
+#include <vector>
+#include <stdint.h>
+
+class DataObject
+{
+	unsigned int startByte;
+	unsigned int size;
+
+public:
+	DataObject(unsigned int startByte, unsigned int lengthInBytes);
+	std::vector<uint8_t> getRawBytes(uint8_t(&saveFile)[SAVEFILE_LENGTH_BYTES]);
+	void setRawBytes(uint8_t(&saveFile)[SAVEFILE_LENGTH_BYTES], uint64_t value);
+	void setRawBytes(uint8_t(&saveFile)[SAVEFILE_LENGTH_BYTES], std::vector<uint8_t> value);
+};
---
+++ src/DataObject.cpp
@@ -0,0 +1,39 @@
+#include "DataObject.h"
+
+DataObject::DataObject(unsigned int startByte, unsigned int lengthInBytes)
+{
+	this->startByte = startByte;
+	this->size = lengthInBytes;
+}
+
+std::vector<uint8_t> DataObject::getRawBytes(uint8_t (&saveFile)[SAVEFILE_LENGTH_BYTES])
+{
+	std::vector<uint8_t> result;
+	for (int i = 0; i < this->size; i++)
+	{
+		result.push_back(saveFile[this->startByte + i]);
+	}
+	return result;
+}
+
+/*
+*	Sets the saveFile bytes associated with this DataObject to the bytes in value
+*	Can set up to 8 bytes, if need to set more use other function with array parameter
+* 
+*	In the case when bytes.size() > this.size(), the additional MSB in bytes are ignored (bytes = 0xABCD -> DataObject = 0xCD for this.size() = 1)
+*	In the case when bytes.size() < this.size(), the additional MSB associated with the DataObject are cleared (bytes = 0xFF -> DataObject = 0x00FF for this.size() = 2)
+* 
+*	This has got to be my favorite function I've created, a wild one-liner
+*/
+void DataObject::setRawBytes(uint8_t (&saveFile)[SAVEFILE_LENGTH_BYTES], uint64_t value)
+{
+	for (int saveFileIdx = this->startByte + this->size - 1; saveFileIdx >= this->startByte; saveFileIdx--, value >>= 8) saveFile[saveFileIdx] = value < 0 ? 0x0 : (value & 0xFF);
+}
+
+/*
+*	Does the same as function above, but can set more than 8 bytes
+*/
+void DataObject::setRawBytes(uint8_t(&saveFile)[SAVEFILE_LENGTH_BYTES], std::vector<uint8_t> value)
+{
+	for (int saveFileIdx = this->startByte + this->size - 1, valueIdx = value.size() - 1; saveFileIdx >= this->startByte; saveFileIdx--, valueIdx--) saveFile[saveFileIdx] = valueIdx < 0 ? 0x0 : value.at(valueIdx);
+}
