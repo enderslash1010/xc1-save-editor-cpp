26f1481b4edb7ebe3b7b91837392eb3c4a17b95d
enderslash1010 <64379743+enderslash1010@users.noreply.github.com>
2024-03-23 15:37:54 -0500

    Added types to DataObjects
    - In the future, these types will be checked upon setting these DataObjects, if some other way is not better

--- include/DataObject.h
+++ include/DataObject.h
@@ -3,13 +3,28 @@
 #include <vector>
 #include <stdint.h>
 
+enum Type
+{
+	UINT8_T,
+	UINT16_T,
+	INT32_T,
+	UINT32_T,
+	BOOL,
+	FLOAT, 
+	STRING,
+	TPL,
+	ARRAY
+};
+
 class DataObject
 {
 	unsigned int startByte;
 	unsigned int size;
+	Type type;
 
 public:
-	DataObject(unsigned int startByte, unsigned int lengthInBytes);
+	DataObject(unsigned int startByte, unsigned int lengthInBytes, Type type);
+
 	std::vector<uint8_t> getRawBytes(uint8_t(&saveFile)[SAVEFILE_LENGTH_BYTES]);
 	void setRawBytes(uint8_t(&saveFile)[SAVEFILE_LENGTH_BYTES], uint64_t value);
 	void setRawBytes(uint8_t(&saveFile)[SAVEFILE_LENGTH_BYTES], std::vector<uint8_t> value);
