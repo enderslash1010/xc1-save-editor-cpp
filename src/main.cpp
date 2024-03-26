4340f9a27fa57149e3b23d02bfaa3bb9d26b0d74
enderslash1010 <64379743+enderslash1010@users.noreply.github.com>
2024-03-25 20:08:52 -0500

    Update main with added array function examples

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,29 @@
 #include "SaveFile.h"
-#include <iostream>
 
 int main()
 {
+
 	try
 	{
 		SaveFile* saveFile = new SaveFile("testSave");
 		std::vector<uint8_t> bytes = saveFile->getRawBytes(THUMSystemSaveFlag);
-		
-		std::cout << "0x";
-		for (uint8_t byte : bytes)
-		{
-			std::cout << (int)byte;
-		}
-		std::cout << '\n';
 
-		saveFile->setRawBytes(ITEMMoney, (uint64_t) 42069);
-		saveFile->setRawBytes(ITEMMoney, { 0x12, 0x34 });
-		saveFile->setValue(ITEMMoney, "abcd");
+		// Setting a save field's raw bytes
+		saveFile->setRawBytes(ITEMMoney, (unsigned int) 42069);
+		saveFile->setRawBytes(ITEMMoney, {0x12, 0x34});
+
+		// Setting a save field using int, string, and float
+		saveFile->setValue(ITEMMoney, (unsigned int) -90);
+		saveFile->setValue(ITEMMoney, "abcdef");
+		saveFile->setValue(ITEMMoney, 123.123f);
+
+		// Setting a save field as an array using an int, string, and float
+		saveFile->setArrayValue(ITEMMoney, 0, 0, (unsigned int) -25);
+		saveFile->setArrayValue(ITEMMoney, 0, 0, "blue");
+		saveFile->setArrayValue(ITEMMoney, 0, 0, 123.123f);
+
+		saveFile->setArrayValue(TBOXArray, 1, TBOXPositionZ, 3.14159f);
+
 		saveFile->saveToFile();
 	}
 	catch (std::exception& e)
