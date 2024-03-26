d6e222187840a17cb06f9fbb7ce24e1c5bbd072c
enderslash1010 <64379743+enderslash1010@users.noreply.github.com>
2024-03-25 20:06:32 -0500

    Added numRows, numColumns, getter functions

--- include/DataObject.h
+++ include/DataObject.h
@@ -2,6 +2,7 @@
 #include "SaveDefines.h"
 #include <vector>
 #include <stdint.h>
+#include <iostream>
 
 enum Type
 {
@@ -18,14 +19,30 @@ enum Type
 
 class DataObject
 {
+protected:
 	unsigned int startByte;
 	unsigned int size;
 	Type type;
 
+	unsigned int numRows, numColumns;
+
 public:
 	DataObject(unsigned int startByte, unsigned int lengthInBytes, Type type);
 
-	std::vector<uint8_t> getRawBytes(uint8_t(&saveFile)[SAVEFILE_LENGTH_BYTES]);
-	void setRawBytes(uint8_t(&saveFile)[SAVEFILE_LENGTH_BYTES], uint64_t value);
-	void setRawBytes(uint8_t(&saveFile)[SAVEFILE_LENGTH_BYTES], std::vector<uint8_t> value);
+	unsigned int getLengthInBytes();
+	Type getType();
+	std::string getTypeStr();
+	unsigned int getStartByte();
+
+	std::vector<uint8_t> getRawBytes(uint8_t(&saveFile)[SAVEFILE_LENGTH_BYTES]) const;
+	void setRawBytes(uint8_t(&saveFile)[SAVEFILE_LENGTH_BYTES], uint64_t value) const;
+	void setRawBytes(uint8_t(&saveFile)[SAVEFILE_LENGTH_BYTES], std::vector<uint8_t> value) const;
+
+	virtual const DataObject* at(unsigned int row, unsigned int column) const
+	{
+		if (row < this->numRows && column < this->numColumns) return this;
+		else return NULL;
+	}
+
+	void print();
 };
--- src/DataObject.cpp
+++ src/DataObject.cpp
@@ -5,9 +5,12 @@ DataObject::DataObject(unsigned int startByte, unsigned int lengthInBytes, Type 
 	this->startByte = startByte;
 	this->size = lengthInBytes;
 	this->type = type;
+
+	this->numRows = 1;
+	this->numColumns = 1;
 }
 
-std::vector<uint8_t> DataObject::getRawBytes(uint8_t (&saveFile)[SAVEFILE_LENGTH_BYTES])
+std::vector<uint8_t> DataObject::getRawBytes(uint8_t (&saveFile)[SAVEFILE_LENGTH_BYTES]) const
 {
 	std::vector<uint8_t> result;
 	for (int i = 0; i < this->size; i++)
@@ -26,7 +29,7 @@ std::vector<uint8_t> DataObject::getRawBytes(uint8_t (&saveFile)[SAVEFILE_LENGTH
 * 
 *	This has got to be my favorite function I've created, a wild one-liner
 */
-void DataObject::setRawBytes(uint8_t (&saveFile)[SAVEFILE_LENGTH_BYTES], uint64_t value)
+void DataObject::setRawBytes(uint8_t (&saveFile)[SAVEFILE_LENGTH_BYTES], uint64_t value) const
 {
 	for (int saveFileIdx = this->startByte + this->size - 1; saveFileIdx >= this->startByte; saveFileIdx--, value >>= 8) saveFile[saveFileIdx] = value < 0 ? 0x0 : (value & 0xFF);
 }
@@ -34,7 +37,69 @@ void DataObject::setRawBytes(uint8_t (&saveFile)[SAVEFILE_LENGTH_BYTES], uint64_
 /*
 *	Does the same as function above, but can set more than 8 bytes
 */
-void DataObject::setRawBytes(uint8_t(&saveFile)[SAVEFILE_LENGTH_BYTES], std::vector<uint8_t> value)
+void DataObject::setRawBytes(uint8_t(&saveFile)[SAVEFILE_LENGTH_BYTES], std::vector<uint8_t> value) const
 {
 	for (int saveFileIdx = this->startByte + this->size - 1, valueIdx = value.size() - 1; saveFileIdx >= this->startByte; saveFileIdx--, valueIdx--) saveFile[saveFileIdx] = valueIdx < 0 ? 0x0 : value.at(valueIdx);
+}
+
+unsigned int DataObject::getLengthInBytes()
+{
+	return this->size;
+}
+
+Type DataObject::getType()
+{
+	return this->type;
+}
+
+unsigned int DataObject::getStartByte()
+{
+	return this->startByte;
+}
+
+std::string DataObject::getTypeStr()
+{
+	std::string result;
+	switch (this->type)
+	{
+	case UINT8_T:
+		result = "UINT8_T";
+		break;
+	case UINT16_T:
+		result = "UINT16_T";
+		break;
+	case UINT32_T:
+		result = "UINT32_T";
+		break;
+	case INT32_T:
+		result = "INT32_T";
+		break;
+	case BOOL:
+		result = "BOOL";
+		break;
+	case FLOAT:
+		result = "FLOAT";
+		break;
+	case STRING:
+		result = "STRING";
+		break;
+	case TPL:
+		result = "TPL";
+		break;
+	case ARRAY:
+		result = "ARRAY";
+		break;
+	default:
+		result = "UNDEFINED";
+		break;
+	}
+	return result;
+}
+
+void DataObject::print()
+{
+	std::cout << "DataObject(" <<
+		"startByte = 0x" << std::hex << this->startByte << ", " <<
+		"lengthInBytes = " << std::dec << this->size << ", " <<
+		"type = " << this->getTypeStr() << ");\n";
 }
