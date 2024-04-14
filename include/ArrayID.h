6c35791fee955a46f0edb68f7ed5d0cfddc5458b
enderslash1010 <64379743+enderslash1010@users.noreply.github.com>
2024-04-14 15:34:51 -0500

    Add enums for element arrays

--- include/ArrayID.h
+++ include/ArrayID.h
@@ -1,5 +1,54 @@
 #pragma once
 
+enum ITEMWeaponArray
+{
+	ITEMWeaponID1,
+	ITEMWeaponStatic1, // 2
+	ITEMWeaponID2,
+	ITEMWeaponStatic2, // 0
+	ITEMWeaponStatic3, // 0
+	ITEMWeaponInventorySlot,
+	ITEMWeaponStatic4, // 1
+	ITEMWeaponStatic5, // 0
+	ITEMWeaponGem1ID,
+	ITEMWeaponGem2ID,
+	ITEMWeaponGem3ID,
+	ITEMWeaponGem4ID,
+	ITEMWeaponGem1Index,
+	ITEMWeaponGem2Index,
+	ITEMWeaponGem3Index,
+	ITEMWeaponGem4Index,
+	ITEMWeaponNumGemSlots,
+	ITEMWeaponStatic6, // 6
+	ITEMWeaponStatic7 // 0
+};
+
+enum ITEMGemArray
+{
+	ITEMGemStatic1, // 0xEA33
+	ITEMGemID1,
+	ITEMGemUnk1,
+	ITEMGemStatic2, // 0
+	ITEMGemInventorySlot,
+	ITEMGemStatic3, // 1
+	ITEMGemStatic4, // 0/3
+	ITEMGemUnk2,
+	ITEMGemValue,
+	ITEMGemRank,
+	ITEMGemUnk3,
+	ITEMGemID2,
+	ITEMGemStatic5, // 2
+	ITEMGemStatic6 // 0
+};
+
+enum MINEArray
+{
+	MINECooldown,
+	MINENumHarvests,
+	MINEMineID,
+	MINEMapID,
+};
+
 enum TBOXElementID
 {
 	TBOXZeroObject,
