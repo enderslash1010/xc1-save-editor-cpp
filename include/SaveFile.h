9fb967de012ea01bbb708a982e574ce7a4aca005
enderslash1010 <64379743+enderslash1010@users.noreply.github.com>
2024-04-14 15:50:53 -0500

    Move byte addressable get/sets to public

--- include/SaveFile.h
+++ include/SaveFile.h
@@ -18,18 +18,18 @@ class SaveFile
     std::string fileLocation;
     uint8_t saveFile[SAVEFILE_LENGTH_BYTES];
 
-    uint8_t getByteAt(unsigned int x);
-    void setByteAt(unsigned int x, uint8_t b);
-
-    std::vector<uint8_t> getRawBytes(DataObject data);
-
     static const DataObject* dataMap[];
 
 public:
     SaveFile(std::string fileLocation);
 
+    uint8_t getByteAt(unsigned int x);
+
+    std::vector<uint8_t> getRawBytes(DataObject data);
     std::vector<uint8_t> getRawBytes(SaveFieldID sfID);
     std::vector<uint8_t> getRawArrayBytes(SaveFieldID sfID, unsigned int index, unsigned int elementName);
+
+    void setByteAt(unsigned int x, uint8_t b);
 
     void setRawBytes(SaveFieldID sfID, std::vector<uint8_t> value);
     void setArrayRawBytes(SaveFieldID aID, unsigned int index, unsigned int elementName, std::vector<uint8_t> value);
